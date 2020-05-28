170e7aa2477d54ad8e99bf6081a9904d39e1220f
viankarivera <viankalrivera@gmail.com>
2020-05-28 13:34:51 -0400

    commit

--- lib/ghibli/cli.rb
+++ lib/ghibli/cli.rb
@@ -17,7 +17,7 @@ class CLI
         if user_input == "yes"
             puts "Great!"
             list_of_films
-            #ask_user_for_film_choice ?
+            ask_user_for_film_choice
         else 
             puts "Sayonara(goodbye)!"
             #binding.pry
@@ -29,14 +29,28 @@ class CLI
         unless index.between?(0,20)
             puts "Sorry, that isn't a film choice."
             index = gets.strip.to_i - 1
+            #after checking for bad inpput
         end 
+            Film.all[index]
+        #find the movie based on index number
+        #display the movie in a method passing find movie object
+            choice = Film.all[index]
+            puts choice.title
+            puts choice.release_date
+            puts choice.director
+            puts choice.description
+            #binding.pry
     end 
 
     def list_of_films
         Film.all.each.with_index(1) do |film, index|
             puts "#{index}. #{film.title}"
         end 
+            puts "Which film are you interested in? Please pick a number from the list."
     end 
+
+    #def find_movie
+     #   Film.all[index]
     
 
     #list film titles?
